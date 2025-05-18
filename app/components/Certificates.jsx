be26e99c0d3978526aab1cc87d0eb6e3d6c5c3f2
MennatAllah <mennamahamed3@gmail.com>
2025-05-19 00:56:34 +0200

    Main Files

--- app/components/Certificates.jsx
+++ app/components/Certificates.jsx
@@ -1,11 +1,6 @@
-"use client";
-
 import Link from "next/link";
-import { useRef } from "react";
 
 export default function Certificates() {
-
-  const certificateRef = useRef(null)
 
   return (
     <main className="mb-14">
@@ -14,31 +9,12 @@ export default function Certificates() {
             <h3 className="text-lg font-bold mb-1">ITI UI/UX Development Track</h3>
             <span className="block w-[80%] mb-4 opacity-90">Information Technology Institute (ITI) – Ministry of Communications and Information Technology</span>
             <span className="block mb-6"><span className="font-bold">Duration: </span>From May 19, 2024 to October 09, 2024</span>
-            <button className="certificate-btn font-bold flex items-center transition-all duration-500 hover:opacity-100 cursor-pointer"
-              onClick={() => {
-                certificateRef.current.classList.remove("hidden")
-              }}>
+            <Link href="/assests/iti-certificate.pdf" target="_blank" className="certificate-btn font-bold flex items-center transition-all duration-500 cursor-pointer">
                 <span>View Certificate</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-3 ms-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                 </svg>
-            </button>
-            <div ref={certificateRef} className="relative hidden">
-              <iframe src="/assests/iti-certificate.pdf" className="w-full h-[400px] lg:h-[600px] ">ITI Certificates</iframe>
-              <Link href="/assests/iti-certificate.pdf" download className="absolute flex items-center font-bold bottom-2.5 left-1/2 -translate-x-1/2 transition-all duration-500 bg-[#0055a6] hover:bg-[#002d81] mb-3 px-10 py-2 rounded-md cursor-pointer">Download
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-3 ms-2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                </svg>
-              </Link>
-              <button className="absolute top-20 right-15 cursor-pointer transition-all duration-500 bg-[#0000006b] hover:bg-red-600 rounded-md "
-                onClick={() => {
-                  certificateRef.current.classList.add("hidden")
-                }}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-8 p-0.5">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
+            </Link>
         </div>
     </main>
   )
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,13 +1,8 @@
-"use client";
-
 import MyProjects from "../../data/data.json";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { useRef } from "react";
 
 export default function Projects() {
-
-    const resumeRef = useRef(null);
 
     const projects = MyProjects.projects.map((project) => (
         <motion.div key={project.id}
@@ -48,31 +43,18 @@ export default function Projects() {
             {projects}
         </div>
         <div className="mt-8 text-white">
-            <button className="resume-btn font-bold flex items-center transition-all duration-500 hover:opacity-100 cursor-pointer"
-                onClick={() => {
-                resumeRef.current.classList.remove("hidden")
-                }}>
+            <Link href="/assests/Mennat-Allah Mahmoud-Resume.pdf" target="_blank" className="resume-btn font-bold flex items-center transition-all duration-500 cursor-pointer">
                 <span>View Resume</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-3 ms-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                 </svg>
-            </button>
-            <div ref={resumeRef} className="relative hidden">
-                <iframe src="/assests/Mennat-Allah Mahmoud-Resume.pdf" className="w-full h-[500px] lg:h-[800px] ">ITI Certificates</iframe>
-                <Link href="/assests/Mennat-Allah Mahmoud-Resume.pdf" download className="absolute flex items-center font-bold bottom-2.5 left-1/2 -translate-x-1/2 transition-all duration-500 bg-[#0055a6] hover:bg-[#002d81] mb-3 px-10 py-2 rounded-md cursor-pointer">Download
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-3 ms-2">
+            </Link>
+            <Link href="/assests/Mennat-Allah Mahmoud-Resume.pdf" download target="_blank" className="download-btn font-bold flex items-center transition-all duration-500 cursor-pointer">
+                <span>Download Resume</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-3 ms-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                 </svg>
-                </Link>
-                <button className="absolute top-20 right-15 cursor-pointer transition-all duration-500 bg-[#0000006b] hover:bg-red-600 rounded-md "
-                onClick={() => {
-                    resumeRef.current.classList.add("hidden")
-                }}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-8 p-0.5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-                </button>
-            </div>
+            </Link>
         </div>
     </main>
   )
